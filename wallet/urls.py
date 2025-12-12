65b197104ecc34acb40081b8876ad529c6e20207
Md. Nazmus Shakib Khan <105985762+Md-Nazmus-Shakib@users.noreply.github.com>
2025-12-13 01:44:54 +0600

    Delete wallet directory

--- wallet/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('wallet/', views.get_wallet, name='get_wallet'),
-    path('balance/', views.get_wallet_by_account, name='get_wallet_by_account'),
-]
